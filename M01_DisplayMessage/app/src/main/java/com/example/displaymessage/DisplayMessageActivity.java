11e482c6f53a7c49f1166228f1ad89b8c3ac7d39
Chris Hughes <chris@chris-hughes.dev>
2024-09-19 23:32:52 -0300

    Fixed the arguments to Calculator class

--- M01_DisplayMessage/app/src/main/java/com/example/displaymessage/DisplayMessageActivity.java
+++ M01_DisplayMessage/app/src/main/java/com/example/displaymessage/DisplayMessageActivity.java
@@ -28,20 +28,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Set the text view as the activity layout
         setContentView(textView);
-// Below are the colors defined in Color.class
-//
-// public static final int BLACK = -16777216;
-// public static final int BLUE = -16776961;
-// public static final int CYAN = -16711681;
-// public static final int DKGRAY = -12303292;
-// public static final int GRAY = -7829368;
-// public static final int GREEN = -16711936;
-// public static final int LTGRAY = -3355444;
-// public static final int MAGENTA = -65281;
-// public static final int RED = -65536;
-// public static final int TRANSPARENT = 0;
-// public static final int WHITE = -1;
-// public static final int YELLOW = -256;
 
         // Set color of container
         switch (message) {
--- M02_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ M02_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.calculator;
 
-import static java.lang.Double.parseDouble;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,58 +14,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Action when "Add" button is pressed
-        Button add = (Button) findViewById(R.id.b_Add);
-        Button subtract = (Button) findViewById(R.id.b_subtract);
-        Button divide = (Button) findViewById(R.id.b_divide);
-        Button multiply = (Button) findViewById(R.id.b_multiply);
+        Button add = findViewById(R.id.b_Add);
+        Button subtract = findViewById(R.id.b_subtract);
+        Button divide =findViewById(R.id.b_divide);
+        Button multiply = findViewById(R.id.b_multiply);
 
         Inputs inputs = new Inputs(
-                    (EditText) findViewById(R.id.num_one),
-                    (EditText) findViewById(R.id.num_two),
-                    (EditText) findViewById(R.id.answer));
+                    findViewById(R.id.num_one),
+                    findViewById(R.id.num_two),
+                    findViewById(R.id.answer));
 
-        add.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.i("Button Press", "Add clicked");
-                Calculate operation = new Calculate(inputs.getInput_one(),
-                        inputs.getInput_two());
+        add.setOnClickListener(v -> {
+            Log.i("Button Press", "Add clicked");
+            Calculate operation = new Calculate(inputs.getInput_one(),
+                    inputs.getInput_two());
 
-                // Set the Answer into the the answer field
-                inputs.getAnswer().setText(String.format("%s", operation.add()));
-            }
+            // Set the Answer into the the answer field
+            inputs.getAnswer().setText(String.format("%s", operation.add()));
         });
 
-        subtract.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.i("Button Press", "Subtract clicked");
-                Calculate operation = new Calculate(parseDouble(String.valueOf(inputs.getInput_one())),
-                        parseDouble(String.valueOf(inputs.getInput_two())));
+        subtract.setOnClickListener(v -> {
+            Log.i("Button Press", "Subtract clicked");
+            Calculate operation = new Calculate(inputs.getInput_one(),
+                    inputs.getInput_two());
 
-                // Set the Answer into the the answer field
-                inputs.getAnswer().setText(String.format("%s", operation.subtract()));
-            }
+            // Set the Answer into the the answer field
+            inputs.getAnswer().setText(String.format("%s", operation.subtract()));
         });
 
-        multiply.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.i("Button Press", "Multiply clicked");
-                Calculate operation = new Calculate(parseDouble(String.valueOf(inputs.getInput_one())),
-                        parseDouble(String.valueOf(inputs.getInput_two())));
+        multiply.setOnClickListener(v -> {
+            Log.i("Button Press", "Multiply clicked");
+            Calculate operation = new Calculate(inputs.getInput_one(),
+                    inputs.getInput_two());
 
-                // Set the Answer into the the answer field
-                inputs.getAnswer().setText(String.format("%s", operation.multiply()));
-            }
+            // Set the Answer into the the answer field
+            inputs.getAnswer().setText(String.format("%s", operation.multiply()));
         });
 
-        divide.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.i("Button Press", "Divide clicked");
-                Calculate operation = new Calculate(parseDouble(String.valueOf(inputs.getInput_one())),
-                        parseDouble(String.valueOf(inputs.getInput_two())));
+        divide.setOnClickListener(v -> {
+            Log.i("Button Press", "Divide clicked");
+            Calculate operation = new Calculate(inputs.getInput_one(),
+                    inputs.getInput_two());
 
-                // Set the Answer into the the answer field
-                inputs.getAnswer().setText(String.format("%s", operation.divide()));
-            }
+            // Set the Answer into the the answer field
+            inputs.getAnswer().setText(String.format("%s", operation.divide()));
         });
 
     }
